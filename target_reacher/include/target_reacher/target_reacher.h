1158311d0a8c6212ea7bda74958300c122d24af6
Kshitij Karnawat <kshitijkarnawat009@gmail.com>
2022-12-16 01:02:06 -0500

    Add files via upload

--- target_reacher/include/target_reacher/target_reacher.h
+++ target_reacher/include/target_reacher/target_reacher.h
@@ -6,6 +6,10 @@
 #include "bot_controller/bot_controller.h"
 #include "ros2_aruco_interfaces/msg/aruco_markers.hpp"
 #include "tf2_ros/transform_broadcaster.h"
+#include <std_msgs/msg/bool.hpp>
+#include "tf2_ros/static_transform_broadcaster.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
 
 // timer
 class TargetReacher : public rclcpp::Node
@@ -15,35 +19,81 @@ public:
     {
 
         m_bot_controller = bot_controller;
+
         auto aruco_target_x = this->declare_parameter<double>("aruco_target.x");
         auto aruco_target_y = this->declare_parameter<double>("aruco_target.y");
-        auto aruco_0_x = this->declare_parameter<double>("final_destination.aruco_0.x");
-        auto aruco_0_y = this->declare_parameter<double>("final_destination.aruco_0.y");
-        auto aruco_1_x = this->declare_parameter<double>("final_destination.aruco_1.x");
-        auto aruco_1_y = this->declare_parameter<double>("final_destination.aruco_1.y");
-        auto aruco_2_x = this->declare_parameter<double>("final_destination.aruco_2.x");
-        auto aruco_2_y = this->declare_parameter<double>("final_destination.aruco_2.y");
-        auto aruco_3_x = this->declare_parameter<double>("final_destination.aruco_3.x");
-        auto aruco_3_y = this->declare_parameter<double>("final_destination.aruco_3.y");
 
-        m_bot_controller->set_goal(aruco_0_x, aruco_0_y);
+        this->declare_parameter<std::string>("final_destination.frame_id");
+
+        this->declare_parameter<double>("final_destination.aruco_0.x");
+        this->declare_parameter<double>("final_destination.aruco_0.y");
+        this->declare_parameter<double>("final_destination.aruco_1.x");
+        this->declare_parameter<double>("final_destination.aruco_1.y");
+        this->declare_parameter<double>("final_destination.aruco_2.x");
+        this->declare_parameter<double>("final_destination.aruco_2.y");
+        this->declare_parameter<double>("final_destination.aruco_3.x");
+        this->declare_parameter<double>("final_destination.aruco_3.y");
+
+        m_bot_controller->set_goal(aruco_target_x, aruco_target_y);
+
+        // subscribes to topic /goal_reached
+        goal_reached_subscriber = this->create_subscription<std_msgs::msg::Bool>("/goal_reached", 10, std::bind(&TargetReacher::goal_reached_callback, this, std::placeholders::_1));
+        
+        // publishes to topic /robot1/odom
+        spin_publisher = this->create_publisher<geometry_msgs::msg::Twist>("/robot1/cmd_vel", 10);
+
+        // subscriber to topic /aruco_marker
+        aruco_subscriber = this->create_subscription<ros2_aruco_interfaces::msg::ArucoMarkers>("/aruco_markers", 10, std::bind(&TargetReacher::aruco_callback, this, std::placeholders::_1));
+
+
+        // final transform
+        final_destination_broadcaster = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+       
+        broadcast_timer = this->create_wall_timer(std::chrono::milliseconds((int)(100.0)), std::bind(&TargetReacher::broadcast_timer_callback, this));
+        
+        final_destination_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+       
+        final_destination_listener = std::make_shared<tf2_ros::TransformListener>(*final_destination_buffer);
 
     }
-    
-    void spin();
-    void find_marker();
-    
-    
 
-// TODO
-// listen to output of set_goal (subscribe to goal_reached/data)
-// spin at location (publish to cmd_vel/angular_velocity and cmd_vel/linear_velocity)
-// find marker (if marker found read id (id is a vector use vector.begin()) and call set_goal())
-// listen to output of set_goal (subscribe to goal_reached/data)
-// find marker (if marker found read id (id is a vector use vector.begin()) and call set_goal())
-// listen to output of set_goal (subscribe to goal_reached/data)
+    void spin();
+    
+    void goal_reached_callback(const std_msgs::msg::Bool::SharedPtr msg);
+    
+    void aruco_callback(const ros2_aruco_interfaces::msg::ArucoMarkers::SharedPtr msg);
+
+    void set_goal();
+
+    void timer_callback();
 
 private:
     // attributes
     std::shared_ptr<BotController> m_bot_controller;
+
+    double final_x{0};
+    double final_y{0};
+
+    int flag{1};
+
+    std::string final_origin;
+
+    // Subscribers
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr goal_reached_subscriber;
+    
+    rclcpp::Subscription<ros2_aruco_interfaces::msg::ArucoMarkers>::SharedPtr aruco_subscriber;
+
+    // Publishers
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr spin_publisher;
+
+    //final transform
+    rclcpp::TimerBase::SharedPtr broadcast_timer;
+    
+    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> final_destination_broadcaster;
+    
+    std::shared_ptr<tf2_ros::TransformListener> final_destination_listener;
+    
+    std::unique_ptr<tf2_ros::Buffer> final_destination_buffer;
+    
 };
+
--- target_reacher/src/target_reacher.cpp
+++ target_reacher/src/target_reacher.cpp
@@ -1,11 +1,91 @@
 #include <rclcpp/rclcpp.hpp>
 #include "target_reacher/target_reacher.h"
-
+#include <string>
 
 void TargetReacher::spin(){
 
+    geometry_msgs::msg::Twist msg;
+    msg.linear.x = 0;
+    msg.linear.y = 0;
+    msg.linear.z = 0;
+    msg.angular.x = 0;
+    msg.angular.y = 0;
+    msg.angular.z = 0.2;
+    spin_publisher->publish(msg);
 }
 
-void TargetReacher::find_marker(){
+void TargetReacher::goal_reached_callback(const std_msgs::msg::Bool::SharedPtr msg){
+   if(msg->data && (flag==1)){
+        spin();
+   }
+}
+
+void TargetReacher::aruco_callback(const ros2_aruco_interfaces::msg::ArucoMarkers::SharedPtr msg){
+    flag = 0;
+    if(msg->marker_ids.at(0) == 0){
+        // get parameters
+        rclcpp::Parameter aruco_0_x = this->get_parameter("final_destination.aruco_0.x");
+        rclcpp::Parameter aruco_0_y = this->get_parameter("final_destination.aruco_0.y");
+        
+        final_x = aruco_0_x.as_double();
+        final_y = aruco_0_y.as_double();
+    }
+
+    if(msg->marker_ids.at(0) == 1){
+        // get parameters
+        rclcpp::Parameter aruco_1_x = this->get_parameter("final_destination.aruco_1.x");
+        rclcpp::Parameter aruco_1_y = this->get_parameter("final_destination.aruco_1.y");
+        
+        final_x = aruco_1_x.as_double();
+        final_y = aruco_1_y.as_double();
+    }
+
+    if(msg->marker_ids.at(0) == 2){
+        // get parameters
+        rclcpp::Parameter aruco_2_x = this->get_parameter("final_destination.aruco_2.x");
+        rclcpp::Parameter aruco_2_y = this->get_parameter("final_destination.aruco_2.y");
+        
+        final_x = aruco_2_x.as_double();
+        final_y = aruco_2_y.as_double();
+    }
+
+    if(msg->marker_ids.at(0) == 3){
+        // get parameters
+        rclcpp::Parameter aruco_3_x = this->get_parameter("final_destination.aruco_3.x");
+        rclcpp::Parameter aruco_3_y = this->get_parameter("final_destination.aruco_3.y");
+        
+        final_x = aruco_3_x.as_double();
+        final_y = aruco_3_y.as_double();
+    }
+    set_goal();
+}
+
+void TargetReacher::timer_callback(){
+    geometry_msgs::msg::TransformStamped tf;
+
+    tf.header.stamp = this->get_clock()->now();
     
+    rclcpp::Parameter origin = this->get_parameter("final_destination.frame_id");
+    final_origin = origin.as_string();
+    
+    tf.header.frame_id = final_origin;
+    tf.child_frame_id = "final_destination";
+    
+    tf.transform.translation.x = final_x;
+    tf.transform.translation.y = final_y;
+    tf.transform.translation.z = 0.0;
+    tf.transform.rotation.x = 0.0;
+    tf.transform.rotation.y = 0.0;
+    tf.transform.rotation.z = 0.0;
+    tf.transform.rotation.w = 1.0;
+
+    final_destination_broadcaster->sendTransform(tf);
+}
+
+void TargetReacher::set_goal(){
+    geometry_msgs::msg::TransformStamped tf;
+    
+    tf = final_destination_buffer->lookupTransform("robot1/odom", "final_destination", tf2::TimePointZero);
+    
+    m_bot_controller->set_goal(tf.transform.translation.x, tf.transform.translation.y);
 }
